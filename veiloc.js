12a25278a5150047498cae26735e9d65ef9fa843
elvy <el.cayro@gmail.com>
2023-10-15 13:06:38 -0300

    ajustes para validar os numeros informados pelo usuario para determinar qtdCadastro, descontoPlanokm, taxaCombPlanokm

--- veiloc.js
+++ veiloc.js
@@ -1,7 +1,7 @@
 /**
  * EXERCÍCIO AULA 1
  *  
- *  Crie um algorítimo para armazenar dados de locação de um veículo.
+ *  Crie um algoritmo para armazenar dados de locação de um veículo.
  * 
  */
 
@@ -15,54 +15,63 @@ let modelo; let ano; let cor; let placa; let renavam; let chassi;
 
 let veiculos = [];
 
+console.log(`
+    § -_-_-_-_- * L * O * C * A * R * -_-_-_-_- §
+        
+            CADASTRE OS VEÍCULOS LOCADOS:       
+`);
+
 let qtdCadastro = prompt("Determine a quantidade de veículos a serem cadastrados: ");
+let descontoPlanokm = prompt("O Plano por Quilometragem tem um desconto no preço da diária. Informe a porcentagem do desconto: ");
+let taxaCombPlanokm = prompt("O Plano por Quilometragem cobra uma taxa sobre o preço do litro do combustível. Informe a porcentagem cobrada sobre o combustível: ");
 
-do{ 
-    modelo = prompt("Modelo: ");
-    ano = prompt("Ano: ");
-    cor = prompt("Cor: ");
-    placa = prompt("Placa: ");
-    renavam = prompt("Renavam: ");
-    chassi = prompt("Chassi: ")
-    precodia = prompt("Preço da diária: R$ ");
-    dias = prompt("Quantidade de dias: ");
-    km = prompt("Quantidade de quilômetros a serem percorridos: ");
-    comb = prompt("Preço do litro do combustível: R$ ");
+if (qtdCadastro>=0 && descontoPlanokm >= 0 && taxaCombPlanokm >= 0) {
+    
+    do {
+        modelo = prompt("Modelo: ");
+        ano = prompt("Ano: ");
+        cor = prompt("Cor: ");
+        placa = prompt("Placa: ");
+        renavam = prompt("Renavam: ");
+        chassi = prompt("Chassi: ")
+        precodia = prompt("Preço da diária: R$ ");
+        dias = prompt("Quantidade de dias: ");
+        km = prompt("Quantidade de quilômetros a serem percorridos: ");
+        comb = prompt("Preço do litro do combustível: R$ ");
 
-    (()=>{
-        planodias = precodia * dias;
-        planodias = planodias.toFixed(2);
-        return planodias;
-    })();
+        (() => {
+            planodias = precodia * dias;
+            return planodias = planodias.toFixed(2);
+        })();
 
-    (()=>{
-        planokm = ((precodia/2)*dias) + ((comb*0.1) * km); 
-        planokm = planokm.toFixed(2);
-        return planokm;
-    })();
+        (() => {
+            planokm = ((precodia * (descontoPlanokm / 100)) * dias) + ((comb * (taxaCombPlanokm / 100)) * km);
+            return planokm = planokm.toFixed(2);
+        })();
 
-    veiculos.push({
-        modelo: modelo,
-        ano: ano,
-        cor: cor,
-        registro:{            
-            placa:placa,
-            renavam:renavam,
-            chassi:chassi
-        },
-        locacao: {
-            precodia: precodia,
-            dias: dias,
-            km:km,
-            comb:comb,
-            planodias: planodias,
-            planokm: planokm
-        }
-    })
+        veiculos.push({
+            modelo: modelo,
+            ano: ano,
+            cor: cor,
+            registro: {
+                placa: placa,
+                renavam: renavam,
+                chassi: chassi
+            },
+            locacao: {
+                precodia: precodia,
+                dias: dias,
+                km: km,
+                comb: comb,
+                planodias: planodias,
+                planokm: planokm
+            }
+        })
 
-} while (veiculos.length < qtdCadastro);
+    } while (veiculos.length < qtdCadastro);
+}
 
-for( v in veiculos){
+for (v in veiculos) {
     console.log(`
         § ------- * L * O * C * A * R * ------- §
            impressiona qualquer maria-gasolina
